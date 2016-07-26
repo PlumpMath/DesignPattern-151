a14b1342f0926d84426d50a3749bfb2db21d6be8
tom <380142431@qq.com>
2016-07-26 23:48:00 +0800

    状态模式 替代丑陋 ，易出错的if else 或者 switch 会增加类的数量

--- app/src/test/java/com/tomchen/designpattern/ExampleUnitTest.java
+++ app/src/test/java/com/tomchen/designpattern/ExampleUnitTest.java
@@ -2,12 +2,17 @@ package com.tomchen.designpattern;
 
 import com.tomchen.designpattern.ProtoType.LoginUserInfo;
 import com.tomchen.designpattern.ProtoType.UserInfo;
+import com.tomchen.designpattern.factory.AudiCar;
+import com.tomchen.designpattern.factory.AudiCarFactory;
+import com.tomchen.designpattern.factory.AudiCarQ2;
+import com.tomchen.designpattern.factory.AudiQ1;
 import com.tomchen.designpattern.rent.Mediator;
 import com.tomchen.designpattern.rent.Tenant;
+import com.tomchen.designpattern.state.TeamUserState;
+import com.tomchen.designpattern.state.oprator.MemberOperator;
+import com.tomchen.designpattern.state.oprator.TeamUserOperator;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -32,5 +37,42 @@ public class ExampleUnitTest {
 
         loginUserInfo.getUserInfo().setStreet("北京");
         System.out.print(loginUserInfo.getUserInfo().toString());
+    }
+
+    @Test
+    public void testWrapLimit(){
+        int[] nums = new int[]{88,11,44,666,5,7,22,4,99};
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if(num > 22 && i != 0){
+                System.out.println();
+            }
+            System.out.print(num+"+");
+        }
+    }
+
+    @Test
+    public void testCreateAudi(){
+        AudiCarFactory audicarfactory = new AudiCarFactory();
+        AudiCar audiCar = audicarfactory.createAudiCar(AudiCarQ2.class);
+        audiCar.drive();
+        audiCar.selfNavigation();
+
+        AudiCar audiCar1 = audicarfactory.createAudiCar(AudiQ1.class);
+        audiCar1.drive();
+        audiCar1.selfNavigation();
+    }
+
+    @Test
+    public void testMemberState(){
+        TeamUserOperator teamUserOperator = new MemberOperator();
+        TeamUserState teamUserState = teamUserOperator.apply();
+        teamUserState.showNoticePage();
+        teamUserState.showWelcomePage();
+
+        teamUserState = teamUserOperator.quitTeam();
+        teamUserState.showWelcomePage();
+        teamUserState.showNoticePage();
+
     }
 }
